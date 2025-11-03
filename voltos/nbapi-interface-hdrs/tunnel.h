b89829da527eedb985ffebebde148d310fd0c20a
Cory <70068963+TwCory@users.noreply.github.com>
2025-11-02 22:53:58 -0500

    Create tunnel.h

---
+++ voltos/nbapi-interface-hdrs/tunnel.h
@@ -0,0 +1,12 @@
+
+typedef u32 tunnel_unit;
+typedef u64 tunnel_key;
+
+typedef enum tunnel_mode tunnel_mode_t;
+typedef enum tunnel_state tunnel_state_t;
+
+typedef struct if_tunnel_s if_tunnel_t;
+
+struct tunnel_ctx;
+struct tunnel_domain;
+struct tunnel_stats;
